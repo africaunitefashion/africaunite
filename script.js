6efd69b2459ba87419fe1423e6bf92e066a7ae59
africaunitefashion <africaunitefashion@gmail.com>
2025-03-17 09:57:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,40 @@ function sendToWhatsApp() {
     var order = document.getElementById("order").value;
     var ownerPhone = "27605505969"; // BABALO
     var url = "https://wa.me/" + ownerPhone + "?text=" + encodeURIComponent("Name: " + name + "\nSurname: " + surname + "\nUser Phone: " + telephone + "\nOrder: " + order);
+    window.open(url, '_blank').focus();const toggleBtn = document.querySelector('.toggle_btn');
+const toggleBtnIcon = document.querySelector('.toggle_btn i');
+const dropDownMenu = document.querySelector('.dropdown_menu');
+
+toggleBtn.onclick = function () {
+    dropDownMenu.classList.toggle('open');
+    const isOpen = dropDownMenu.classList.contains('open');
+    toggleBtnIcon.classList = isOpen
+    ? 'fa-solid fa-xmark'
+    : 'fa-solid fa-bars'
+}
+
+function sendToWhatsApp() {
+    var name = document.getElementById("name").value;
+    var surname = document.getElementById("surname").value;
+    var telephone = document.getElementById("telephone").value;
+    var order = document.getElementById("order").value;
+    var ownerPhone = "27605505969"; // BABALO
+    var url = "https://wa.me/" + ownerPhone + "?text=" + encodeURIComponent("Name: " + name + "\nSurname: " + surname + "\nUser Phone: " + telephone + "\nOrder: " + order);
     window.open(url, '_blank').focus();
+  }
+  
+  window.addEventListener('DOMContentLoaded', function () {
+    const prices = document.querySelectorAll('.price');
+
+    prices.forEach(price => {
+        if (price.classList.contains('shirt-price')) {
+            price.textContent = 'T-Shirt • R399'; 
+        } else if (price.classList.contains('cap-price')) {
+            price.textContent = 'Cap • R299'; 
+        }
+    });
+});
+
   }
   
   window.addEventListener('DOMContentLoaded', function () {
